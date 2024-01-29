e1ea681224f9e90aba0c7067e251f7ce939d274c
vkomodey <vkomodey@gmail.com>
2024-01-30 00:08:11 +0100

    hashing passwords with argon2

--- src/modules/user/use-cases/register/register.use-case.ts
+++ src/modules/user/use-cases/register/register.use-case.ts
@@ -1,16 +1,17 @@
-import {v4 as uuid} from 'uuid';
-import {Inject, Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import * as argon2 from 'argon2';
 
-import {UseCase} from 'src/lib/application/interfaces/use-case';
-import {UserEntity} from '../../domain/user.entity';
 import { UserRepository } from '../../database/user.repository';
+import { UserEntity } from '../../domain/user.entity';
 
 @Injectable()
 export class RegisterUseCase {
   constructor(private userRepository: UserRepository) {}
   async execute(firstName: string, lastName: string, email: string, phone: string, password: string): Promise<void> {
 
-    const userEntity = new UserEntity(uuid(), firstName, lastName, email, phone, password) ;
+    const hashedPassword = await argon2.hash(password);
+    const userEntity = new UserEntity(uuid(), firstName, lastName, email, phone, hashedPassword) ;
 
     this.userRepository.save(userEntity);
   }
