824e4a0fde32a1b0125584e85917ea5e73a79fca
Vlad Komodey <vkomodey@gmail.com>
2024-04-27 21:06:58 -0500

    Fix tests

--- src/modules/api/api.module.ts
+++ src/modules/api/api.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { APIController } from './api.controller';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [APIController],
-  imports: [
-    UserModule,
-    AuthModule,
-  ],
+  imports: [UserModule, AuthModule],
 })
 export class APIModule {}
--- src/modules/user/domain/user.entity.spec.ts
+++ src/modules/user/domain/user.entity.spec.ts
@@ -1,43 +1,95 @@
-import { User } from "../database/user.entity";
-import { UserEntity } from "./user.entity";
-
+import { UserEntity } from './user.entity';
 
 describe('UserEntity', () => {
   describe('Create', () => {
     it('Should create a user entity', () => {
-      new UserEntity('id', 'Jets', 'Nano', 'jetson@nano.com', '+123454323423', new Date(), new Date());
+      new UserEntity(
+        'id',
+        'Jets',
+        'Nano',
+        'jetson@nano.com',
+        '+123454323423',
+        'password',
+        new Date(),
+        new Date(),
+      );
     });
 
     it('Should create a user without proper created/updated dates', () => {
       new UserEntity('id', 'Jets', 'Nano', 'jetson@nano.com', '+123454323423');
-    })
+    });
 
     it('Should not create a user without a name', () => {
       expect(() => {
-        new UserEntity('id', 'Jets', null, 'jetson@nano.com', '+123454323423', new Date(), new Date());
+        new UserEntity(
+          'id',
+          'Jets',
+          null,
+          'jetson@nano.com',
+          '+123454323423',
+          'password',
+          new Date(),
+          new Date(),
+        );
       }).toThrow();
     });
 
-    it('Should create a user without id(in case it\'s a new one)', () => {
-      new UserEntity(null, 'Jets', 'Nano', 'jetson@nano.com', '+123454323423', new Date(), new Date());
+    it("Should create a user without id(in case it's a new one)", () => {
+      new UserEntity(
+        null,
+        'Jets',
+        'Nano',
+        'jetson@nano.com',
+        '+123454323423',
+        'password',
+        new Date(),
+        new Date(),
+      );
     });
 
     it('Should not create a user without an email', () => {
       expect(() => {
-        new UserEntity('id', 'Jets', 'Nano', null, '+123454323423', new Date(), new Date());
+        new UserEntity(
+          'id',
+          'Jets',
+          'Nano',
+          null,
+          '+123454323423',
+          'password',
+          new Date(),
+          new Date(),
+        );
       }).toThrow();
     });
 
     it('Should not create a user without a phone', () => {
       expect(() => {
-        new UserEntity('id', 'Jets', 'Nano', 'jetson@nano.com', null, new Date(), new Date());
+        new UserEntity(
+          'id',
+          'Jets',
+          'Nano',
+          'jetson@nano.com',
+          null,
+          'password',
+          new Date(),
+          new Date(),
+        );
       }).toThrow();
     });
 
-    it('Should change user\'s first name', () => {
-      let user = new UserEntity('id', 'Jets', 'Nano', 'jetson@nano.com', '+123454323423', new Date(), new Date());
+    it("Should change user's first name", () => {
+      const user = new UserEntity(
+        'id',
+        'Jets',
+        'Nano',
+        'jetson@nano.com',
+        '+123454323423',
+        'password',
+        new Date(),
+        new Date(),
+      );
 
       user.firstName = 'arduino@iot.com';
     });
-  })
+  });
 });
